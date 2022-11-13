e21790cab1881381f0109c8668f746e86c73983b
umanizer <arata1005@yahoo.co.jp>
2022-11-13 16:10:07 +0900

    ⚗️ 不要な記述でいつか消しますが試しにクリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,26 @@ import styles from '../styles/Home.module.css';
 import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
+const foo =1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  },[]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
