dac1b1790ea110f5c755ee538006a1e4c1a86540
umanizer <arata1005@yahoo.co.jp>
2022-11-13 13:16:52 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,36 +1,39 @@
 import Image from 'next/image';
 import classes from './Links.module.css';
 
-export  function Links() {
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation \u2192',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn \u2192',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/canary/examples',
+    title: 'Examples \u2192',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy \u2192',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
+export function Links() {
   return (
     <div className={classes.grid}>
-      <a href="https://nextjs.org/docs" className={classes.card}>
-        <h2 className={classes.title}>Documentation &rarr;</h2>
-        <p className={classes.description}>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/learn" className={classes.card}>
-        <h2 className={classes.title}>Learn &rarr;</h2>
-        <p className={classes.description}>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href="https://github.com/vercel/next.js/tree/canary/examples"
-        className={classes.card}
-      >
-        <h2 className={classes.title}>Examples &rarr;</h2>
-        <p className={classes.description}>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={classes.card}
-      >
-        <h2 className={classes.title}>Deploy &rarr;</h2>
-        <p className={classes.description}>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {ITEMS.map((item) => (
+        <a key={item.title} href={item.href} className={classes.card}>
+          <h2 className={classes.title}>{item.title}</h2>
+          <p className={classes.description}>{item.description}</p>
+        </a>
+      ))}
     </div>
   );
 }
