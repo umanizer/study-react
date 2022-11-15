e598a38bbabd271de89e04911cb69eb9e82f088d
umanizer <arata1005@yahoo.co.jp>
2022-11-15 22:22:45 +0900

    ✨ コンポーネント間のリフトアップについて勉強

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,34 +1,9 @@
-import Image from 'next/image';
 import classes from './Links.module.css';
 
-const ITEMS = [
-  {
-    href: 'https://nextjs.org/docs',
-    title: 'Documentation \u2192',
-    description: 'Find in-depth information about Next.js features and API.',
-  },
-  {
-    href: 'https://nextjs.org/learn',
-    title: 'Learn \u2192',
-    description: 'Learn about Next.js in an interactive course with quizzes!',
-  },
-  {
-    href: 'https://github.com/vercel/next.js/tree/canary/examples',
-    title: 'Examples \u2192',
-    description: 'Discover and deploy boilerplate example Next.js projects.',
-  },
-  {
-    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-    title: 'Deploy \u2192',
-    description:
-      'Instantly deploy your Next.js site to a public URL with Vercel.',
-  },
-];
-
-export function Links() {
+export function Links({ items }) {
   return (
     <div className={classes.grid}>
-      {ITEMS.map((item) => (
+      {items.map((item) => (
         <a key={item.title} href={item.href} className={classes.card}>
           <h2 className={classes.title}>{item.title}</h2>
           <p className={classes.description}>{item.description}</p>
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,46 @@
-import { useEffect } from 'react';
+import { useCallback, useState } from 'react';
 import { Headline } from 'src/components/Headline';
 import { Links } from 'src/components/Links';
 import classes from 'src/components/Main/Main.module.css';
 
-export function Main(props) {
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation \u2192',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn \u2192',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/canary/examples',
+    title: 'Examples \u2192',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy \u2192',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
 
+export function Main(props) {
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, -1);
+    });
+  }, []);
 
   return (
     <main className={classes.main}>
-      <Headline page={props.page}>
-        <code className={classes.code}>pages/{props.page}.js</code>
+      <Headline page={props.page} handleReduce={handleReduce}>
+        <code className={classes.code}>{items.length}</code>
       </Headline>
-      <Links />
+      <Links items={items}/>
     </main>
   );
 }
