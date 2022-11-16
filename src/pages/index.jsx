f01c92fc84e47e404e5799e9b397ba0a5f0c366e
umanizer <arata1005@yahoo.co.jp>
2022-11-16 21:09:33 +0900

    ✨ APIを叩いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,37 @@ import styles from '../styles/Home.module.css';
 import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { useCallback, useEffect, useState } from 'react';
 
-const  Home = (props) => {
+const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  console.log(posts);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {props.isShow ? <h1>{props.count}</h1> : null}
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
+
+      {/* {props.isShow ? <h1>{props.count}</h1> : null}
       <button onClick={props.handleClick}>ボタン</button>
       <button onClick={props.handleDisplay}>
         {props.isShow ? '非表示' : '表示'}
@@ -25,8 +47,8 @@ const  Home = (props) => {
       </ul>
 
       <Main page="index" />
-      <Footer />
+      <Footer /> */}
     </div>
   );
-}
+};
 export default Home;
